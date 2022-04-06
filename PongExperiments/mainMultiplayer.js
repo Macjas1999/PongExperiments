0269244db297ec715004813a4f1b0d91f45e072a
Maciej <MaciejJ1999@interia.pl>
2022-04-06 19:57:18 +0200

    Movement logick embeded in functions

--- PongExperiments/mainMultiplayer.js
+++ PongExperiments/mainMultiplayer.js
@@ -176,6 +176,107 @@ function drawScore(){
     ctx.fillText(lrScore, canvas.width/2, canvas.height - canvas.height/4)
 }
 
+//Logic section
+
+// function incrementScoreAD(){
+//     //increment score and reset ball position and pause the game
+//     adScore++;
+//     x = canvas.width/2;
+//     y = canvas.height/2;
+//     paused = true;
+// }
+
+// function incrementScoreLR(){
+//     //increment score and reset ball position and pause the game
+//     lrScore++;
+//     x = canvas.width/2;
+//     y = canvas.height/2;
+//     paused = true;
+// }
+function incrementScore(whichPaddle){
+    x = canvas.width/2;
+    y = canvas.height/2;
+    paused = true;
+    if(whichPaddle = 1){
+        adScore++;
+    }
+    else if (whichPaddle = 0){
+        lrScore++;
+    }
+}
+
+function movementPaddleYDir(){
+    if(upDownPressed && upPaddleYOffset < maxYOffset + 7){
+        upPaddleYOffset += 5;
+    }
+    else if(upUpPressed && upPaddleYOffset > 7){
+        upPaddleYOffset -= 5;
+    }
+    
+    if(downDownPressed && downPaddleYOffset > 7){
+        downPaddleYOffset -= 5;
+    }
+    else if(downUpPressed && downPaddleYOffset < maxYOffset + 7){
+        downPaddleYOffset += 5;
+    }
+}
+
+function movementPaddleXDir(){
+    if(upRightPressed && upPaddleX < canvas.width-upPaddleWidth) {
+        upPaddleX += 7;
+    }
+    else if(upLeftPressed && upPaddleX > 0) {
+        upPaddleX -= 7;
+    }
+    
+    if(downRightPressed && downPaddleX < canvas.width-downPaddleWidth) {
+        downPaddleX += 7;
+    }
+    else if(downLeftPressed && downPaddleX > 0) {
+        downPaddleX -= 7;
+    }
+}
+function ballMovementAndCollision(){
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    
+    if(y + dy < ballRadius+upPaddleHeight) {
+        if(x > upPaddleX && x < upPaddleX + upPaddleWidth) { // paddle offset lll
+            dy = -dy;
+            
+        }
+        else {
+            incrementScore(0);
+        }
+    }
+    if(y + dy > canvas.height-ballRadius-downPaddleHeight) {/// && y + dy < canvas.height-ballRadius-downPaddleHeight
+        if(x > downPaddleX && x < downPaddleX + downPaddleWidth) {
+            dy = -dy;
+        }
+        else {
+            incrementScore(1);
+        }
+    }
+
+    if(y + dy < upPaddleYOffset + ballRadius*2 && y + dy > upPaddleYOffset - ballRadius*2 && x + dx > upPaddleX && x + dx < upPaddleX + upPaddleWidth) {
+        if(!(y+dy > y)){//!(y+dy > y)
+            dy = -dy;
+            lastUp = true;
+            lastDown = false;
+        }
+    
+    }
+    if(y + dy < canvas.height - downPaddleYOffset + ballRadius*2 && y + dy > canvas.height - downPaddleYOffset - ballRadius*2 && x + dx > downPaddleX && x + dx < downPaddleX + downPaddleWidth) {
+        if(!(y+dy < y)){
+            dy = -dy;
+            lastDown = true;
+            lastUp = false;
+        }
+    }
+
+}
+
 //Mainfunciton###################
 function drawMultiplayer() {
     if(paused){return;}
@@ -185,95 +286,16 @@ function drawMultiplayer() {
     drawPaddleDown();
     drawPaddleUp();
     drawScore();
-
-    //killines
-    if(y + dy > canvas.height-ballRadius/2) {
-        adScore++;
-        x = canvas.width/2
-        y = canvas.height/2;
-        paused = true;
-    }
-    else if(y + dy < 0+ballRadius/2) {
-        lrScore++;
-        x = canvas.width/2
-        y = canvas.height/2;
-        paused = true;
-    }
-
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-        dx = -dx;
-    }
-
-    if(y + dy < ballRadius+upPaddleHeight) {
-        if(x > upPaddleX && x < upPaddleX + upPaddleWidth) {
-            dy = -dy;
-            
-        }
-        else {
-            lrScore++;
-            x = canvas.width/2;
-            y = canvas.height/2;
-            paused = true;
-        }
-    }
-    if(y + dy > canvas.height-ballRadius-downPaddleHeight) {
-        if(x > downPaddleX && x < downPaddleX + downPaddleWidth) {
-            dy = -dy;
-        }
-        else {
-            adScore++;
-            x = canvas.width/2;
-            y = canvas.height/2;
-            paused = true;
-        }
-    }
     
-    if(upRightPressed && upPaddleX < canvas.width-upPaddleWidth) {
-        upPaddleX += 7;
-    }
-    else if(upLeftPressed && upPaddleX > 0) {
-        upPaddleX -= 7;
-    }
-
-    if(downRightPressed && downPaddleX < canvas.width-downPaddleWidth) {
-        downPaddleX += 7;
-    }
-    else if(downLeftPressed && downPaddleX > 0) {
-        downPaddleX -= 7;
-    }
-
-    //yOffset
-    if(y + dy < upPaddleYOffset + ballRadius*2 && y + dy > upPaddleYOffset - ballRadius*2 && x + dx > upPaddleX && x + dx < upPaddleX + upPaddleWidth) {
-        if(!lastUp){
-            dy = -dy;
-            lastUp = true;
-            lastDown = false;
-        }
-    }
-    if(y + dy < canvas.height - downPaddleYOffset + ballRadius*2 && y + dy > canvas.height - downPaddleYOffset - ballRadius*2 && x + dx > downPaddleX && x + dx < downPaddleX + downPaddleWidth) {
-        if(!lastDown){
-            dy = -dy;
-            lastDown = true;
-            lastUp = false;
-        }
-    }
-    if(upDownPressed && upPaddleYOffset < maxYOffset + 7){
-        upPaddleYOffset += 5;
-    }
-    else if(upUpPressed && upPaddleYOffset > 7){
-        upPaddleYOffset -= 5;
-    }
-
-
-    if(downDownPressed && downPaddleYOffset > 7){
-        downPaddleYOffset -= 5;
-    }
-    else if(downUpPressed && downPaddleYOffset < maxYOffset + 7){
-        downPaddleYOffset += 5;
-    }
+    //Logic section
+    movementPaddleXDir();
+    movementPaddleYDir();
+    ballMovementAndCollision();
+    //killines
     
     x += dx;
     y += dy;
+    //End of logick istructions
 
     requestAnimationFrame(drawMultiplayer);
 }
---
+++ testLogickTemplate.js
@@ -0,0 +1,108 @@
+//Description
+//Killines
+function incrementScoreAD(){
+    //increment score and reset ball position and pause the game
+    adScore++;
+    x = canvas.width/2;
+    y = canvas.height/2;
+    paused = true;
+}
+
+function incrementScoreLR(){
+    //increment score and reset ball position and pause the game
+    lrScore++;
+    x = canvas.width/2;
+    y = canvas.height/2;
+    paused = true;
+}
+function incrementScore(whichPaddle){
+    x = canvas.width/2;
+    y = canvas.height/2;
+    paused = true;
+    if(whichPaddle = 1){
+        adScore++;
+    }
+    else if (whichPaddle = 0){
+        lrScore++;
+    }
+}
+
+function movementPaddleYDir(){
+    if(upDownPressed && upPaddleYOffset < maxYOffset + 7){
+        upPaddleYOffset += 5;
+    }
+    else if(upUpPressed && upPaddleYOffset > 7){
+        upPaddleYOffset -= 5;
+    }
+    
+    if(downDownPressed && downPaddleYOffset > 7){
+        downPaddleYOffset -= 5;
+    }
+    else if(downUpPressed && downPaddleYOffset < maxYOffset + 7){
+        downPaddleYOffset += 5;
+    }
+}
+
+function movementPaddleXDir(){
+    if(upRightPressed && upPaddleX < canvas.width-upPaddleWidth) {
+        upPaddleX += 7;
+    }
+    else if(upLeftPressed && upPaddleX > 0) {
+        upPaddleX -= 7;
+    }
+    
+    if(downRightPressed && downPaddleX < canvas.width-downPaddleWidth) {
+        downPaddleX += 7;
+    }
+    else if(downLeftPressed && downPaddleX > 0) {
+        downPaddleX -= 7;
+    }
+}
+function ballMovementAndCollision(){
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    
+    if(y + dy < ballRadius+upPaddleHeight) {
+        if(x > upPaddleX && x < upPaddleX + upPaddleWidth) { // paddle offset lll
+            dy = -dy;
+            
+        }
+        else {
+            incrementScore(0);
+        }
+    }
+    if(y + dy > canvas.height-ballRadius-downPaddleHeight) {/// && y + dy < canvas.height-ballRadius-downPaddleHeight
+        if(x > downPaddleX && x < downPaddleX + downPaddleWidth) {
+            dy = -dy;
+        }
+        else {
+            incrementScore(1);
+        }
+    }
+
+    if(y + dy < upPaddleYOffset + ballRadius*2 && y + dy > upPaddleYOffset - ballRadius*2 && x + dx > upPaddleX && x + dx < upPaddleX + upPaddleWidth) {
+        if(!(y+dy > y)){//!(y+dy > y)
+            dy = -dy;
+            lastUp = true;
+            lastDown = false;
+        }
+    
+    }
+    if(y + dy < canvas.height - downPaddleYOffset + ballRadius*2 && y + dy > canvas.height - downPaddleYOffset - ballRadius*2 && x + dx > downPaddleX && x + dx < downPaddleX + downPaddleWidth) {
+        if(!(y+dy < y)){
+            dy = -dy;
+            lastDown = true;
+            lastUp = false;
+        }
+    }
+
+}
+
+
+
+
+
+
+x += dx;
+y += dy;
